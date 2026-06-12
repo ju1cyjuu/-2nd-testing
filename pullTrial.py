bbf746bcdf97e5e99c9b933cd4d6e6c1b9d03969
ju1cyjuu <kyinuukhinjulia@gmail.com>
2026-06-12 14:44:26 +0630

    12 June Final commit

--- pullTrial.py
+++ pullTrial.py
@@ -1,11 +1,26 @@
 num_list= [20,50,66,80,10,5]
 userInput= int(input("Enter a search number: "))
+# for i in range(len(num_list)):
+
+#     if userInput == num_list[i]:
+#         print(f"The number in found in the list position {i+1}.")
+#         break 
+
+# else: 
+#     print("The number is not in the list.")
+condition = False
+index = 0
 for i in range(len(num_list)):
 
     if userInput == num_list[i]:
-        print(f"The number in found in the list position {i+1}.")
-        break
+        condition = True
+        index= i
+        break 
 
-else: 
-    print("The number is not in the list.")
+    else: 
+        condition = False
 
+if condition == True:
+    print(f"The number is found in the list, position {index+1}")
+else:
+    print("The number is not in the range.")
