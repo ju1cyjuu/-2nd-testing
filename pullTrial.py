b1aedc3d7a0f0c898b794d75752535ff3e09208c
ju1cyjuu <kyinuukhinjulia@gmail.com>
2026-06-12 14:31:44 +0630

    first program commit

--- pullTrial.py
+++ pullTrial.py
@@ -1,1 +1,11 @@
-print("Pulling data")
+num_list= [20,50,66,80,10,5]
+userInput= int(input("Enter a search number: "))
+for i in range(len(num_list)):
+
+    if userInput == num_list[i]:
+        print(f"The number in found in the list position {i+1}.")
+        break
+
+else: 
+    print("The number is not in the list.")
+
