9d5ca1bd99e426483372f3ac0cacd19ba53fce04
ju1cyjuu <kyinuukhinjulia@gmail.com>
2026-06-17 12:05:53 +0630

    retry

---
+++ BubbleSort.py
@@ -0,0 +1,14 @@
+#Bubble sort
+class BubbleSort:
+    @staticmethod
+    def sort(arr):
+        n = len(arr)
+        for i in range(n-1):
+            for j in range(n- 1 - i):
+                if arr[j] > arr[j + 1]:
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]  # Swap elements
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+print("Unsorted array:", arr)
+BubbleSort.sort(arr)
+print("Sorted array:", arr)
